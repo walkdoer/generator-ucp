35556dcb28824078a778aed3a9ab87b1c50f3aea
zhangmh <zhangmhao@gmail.com>
2014-02-26 16:23:43 +0800

    添加文件依赖关系

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -159,6 +159,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     //bower the denpendencies
     grunt.registerTask('bower', 'bowercopy');
     //build project
